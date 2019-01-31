55dffa18d8ec2e9d4a6eab662a13fb7520a0afef
Romy Maghsoudi <romy_mag@outlook.com>
2019-01-31 03:30:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
-function theBeatlesPlay(array1, array2){
-  var emptyArray = [];
-  for(let i = 0; array1.length > 0; i++){
-    
-  }
+function theBeatlesPlay (musicians, instruments) {
+
+var MyArray = [];
+
+for (let i = 0; i < musicians.length; i++) {
+  MyArray.push(musicians[i] + " plays " + instruments[i])
+}
+return MyArray;
+}// add solution here
+
+function johnLennonFacts (array) {
+let newArray = []
+var n = 0;
+  while (n < array.length) {
+    newArray.push(array[n] +"!!!");
+    n++
+}
+return newArray;
+}
+
+function iLoveTheBeatles (n) {
+var emptyArray = [];
+let i = n;
+do {
+  emptyArray.push("I love the Beatles!")
+  i++;
+} while (i < 15);
+return emptyArray;
 }
